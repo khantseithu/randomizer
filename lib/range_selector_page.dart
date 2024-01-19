92515b67bbbc615d481d95e4e797158fe0d67219
khantseithuu <khantseithuu@gmail.com>
2024-01-19 16:32:34 +0000

    initial range selector page

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -10,6 +10,28 @@ class RangeSelectorPage extends StatefulWidget {
 class _RangeSelectorPageState extends State<RangeSelectorPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          "Select Range",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        )),
+        backgroundColor: Colors.blue,
+      ),
+      body: Form(
+        child: Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_forward),
+        onPressed: () {
+          // TODO: Validate the form
+          // TODO: Navigate to generate number page
+        },
+      ),
+    );
   }
 }
